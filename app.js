9ec84bfd3879265b7685f16b300999df912cb1af
Brian <rodriguezrbrian@gmail.com>
2022-08-26 18:34:00 -0300

    Fix html

--- app.js
+++ app.js
@@ -4,40 +4,45 @@ const path = require('path');
 
 const PORT = 3030
 
-app.use(express.static('./public/img'));
+app.use(express.static('public/img'));
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname,'/views/index.html'))
-});
+})
 
-app.get('/babbage',function(req,res){
+app.get('/index.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+
+app.get('/babbage.html',function(req,res){
     res.sendFile(path.join(__dirname,'/views/babbage.html'))
-});
+})
 
-app.get('/berners-lee',function(req,res){
+app.get('/berners-lee.html',function(req,res){
     res.sendFile(path.join(__dirname,'/views/berners-lee.html'))
-});
+})
 
-app.get('/hamilton',function(req,res){
+app.get('/hamilton.html',function(req,res){
     res.sendFile(path.join(__dirname,'/views/hamilton.html'))
 })
 
-app.get('/clarke',function(req,res){
+app.get('/clarke.html',function(req,res){
     res.sendFile(path.join(__dirname,'/views/clarke.html'))
 })
 
-app.get('/hopper',function(req,res){
+app.get('/hopper.html',function(req,res){
     res.sendFile(path.join(__dirname,'/views/hopper.html'))
 })
 
-app.get('/lovelace',function(req,res){
+app.get('/lovelace.html',function(req,res){
     res.sendFile(path.join(__dirname,'/views/lovelace.html'))
 })
 
-app.get('/turing',function(req,res){
+app.get('/turing.html',function(req,res){
     res.sendFile(path.join(__dirname,'/views/turing.html'))
 })
 
-app.listen(3030,function(){
+app.listen(PORT,function(){
     console.log('Servidor Levantado');
 })
